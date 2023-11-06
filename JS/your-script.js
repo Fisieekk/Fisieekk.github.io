29e5a4a243d2a6d10474fceee1a199d23b877756
Marcin Serafin <Marcin.serafin0325@gmail.com>
2023-11-06 10:18:23 +0100

    some changes

--- JS/your-script.js
+++
@@ -1,4 +0,0 @@
-document.querySelector('.mobile-menu-button').addEventListener('click', function () {
-    const mobileMenu = document.querySelector('.mobile-menu');
-    mobileMenu.style.display = (mobileMenu.style.display === 'block') ? 'none' : 'block';
-});
